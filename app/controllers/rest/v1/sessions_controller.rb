2d45fbcf38ff74692d4adefe4cb7bb67bd6361eb
Federico Moyá <federicomoyamartin@gmail.com>
2019-02-13 22:10:52 -0300

    Set up use of branch for generating deep links (#4)

--- app/controllers/rest/v1/sessions_controller.rb
+++ app/controllers/rest/v1/sessions_controller.rb
@@ -4,20 +4,43 @@ module Rest
     	skip_before_action :verify_authenticity_token
 
     	def create
-	    	command = AuthenticateUser.call(params[:email], params[:password])
-			  if command.success?
+        byebug
+        command = AuthenticateUser.call(params[:email], params[:password], params[:code])
+        if command.success?
           u = User.where(email: params[:email]).first
-			    render json: { 
+          render json: { 
             auth_token: command.result,
             user: u.attributes.slice('id', 'first_name', 'last_name', 'email')
           }
-				else
-			    render json: { error: command.errors }, status: :unauthorized
-			  end
+        else
+          render json: { error: command.errors }, status: :unauthorized
+        end
     	end
 
       def destroy
       end
+
+      def deep_link_mail
+        if params[:email]
+          user = User.find_by(email: params[:email])          
+          render :unauthorized unless user
+          branch_service = BranchService.new
+          deep_link = branch_service.get_client.link(channel:'decify-api', 
+            feature:'login', 
+            data:{
+              code:  SecureRandom.hex(10),
+              email: params[:email]
+            }
+          ).url
+          
+          UserMailer.new_mobile_session(user, deep_link).deliver_now
+
+          head :no_content
+        else
+          render :unauthorized
+        end
+      end
+      
     end
   end
 end
