687ea593090f2aa889876c0d289c867b0c99862c
Federico Moyá <federicomoyamartin@gmail.com>
2019-02-17 23:03:19 -0300

    Update resources to allow user sign up (#6)

--- app/controllers/rest/v1/users_controller.rb
+++ app/controllers/rest/v1/users_controller.rb
@@ -1,6 +1,7 @@
 module Rest
   module V1
     class UsersController < ApiController
+    	skip_before_action :authenticate_request, only: [:create]
     end
   end
 end
--- app/resources/rest/v1/user_resource.rb
+++ app/resources/rest/v1/user_resource.rb
@@ -2,7 +2,7 @@ module Rest
   module V1
     class UserResource < ApplicationResource
 
-      attributes :email, :first_name, :last_name
+      attributes :email, :first_name, :last_name, :password, :user_type
       has_many :decisions
 
     end
