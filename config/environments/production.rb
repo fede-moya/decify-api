6b30d2c5c608d3d3702683cd6415dac33318bec8
federicomoyamartin <federicomoyamartin@gmail.com>
2019-03-23 13:24:21 -0300

    Update gmail client

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,13 +85,13 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'federicomoyamartin@gmail.com'}
+  config.action_mailer.default_options = {from: 'federicomoya94@gmail.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'decify-api.herokuapp.com',
-    user_name:            'federicomoyamartin',
+    user_name:            'federicomoya94',
     password:             Rails.application.secrets.gmail_client_password,
     authentication:       'plain',
     enable_starttls_auto: true  
