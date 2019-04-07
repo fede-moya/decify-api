a8691c5b7c178da663c90f5edb261e148060dcae
federicomoyamartin <federicomoyamartin@gmail.com>
2019-04-07 17:07:45 -0300

    Resolve sessions controller bug that was casusing a 500 server error

--- app/controllers/rest/v1/sessions_controller.rb
+++ app/controllers/rest/v1/sessions_controller.rb
@@ -1,27 +1,30 @@
 module Rest
   module V1
     class SessionsController < ApplicationController
-    	skip_before_action :verify_authenticity_token
+      skip_before_action :verify_authenticity_token
 
-    	def create
+      def create
         command = AuthenticateUser.call(params[:email], params[:password], params[:code])
         if command.success?
           u = User.where(email: params[:email]).first
-          render json: { 
+          render json: {
             auth_token: command.result,
             user: u.attributes.slice('id', 'first_name', 'last_name', 'email')
           }
         else
           render json: { error: command.errors }, status: :unauthorized
         end
-    	end
-
-      def destroy
       end
+
+      def destroy; end
 
       def deep_link_mail
         user = User.find_by_email(params[:email])
-        render :unauthorized unless user
+
+        unless user.present?
+          head :unauthorized
+          return
+        end
 
         user.update(authorization_code: SecureRandom.hex(10))
 
