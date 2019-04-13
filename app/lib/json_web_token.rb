814144f07cf34950d9529ef3491ecc3e59a227e1
Federico Moyá <federicomoyamartin@gmail.com>
2019-04-13 15:51:30 -0300

    Increase token expiration time (#17)

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -1,15 +1,16 @@
 class JsonWebToken
- class << self
-   def encode(payload, exp = 24.hours.from_now)
-     payload[:exp] = exp.to_i
-     JWT.encode(payload, Rails.application.secrets.secret_key_base)
-   end
+  TOKEN_EXPIRATION_TIME = (365 * 24).hours.from_now
+  class << self
+    def encode(payload, exp = TOKEN_EXPIRATION_TIME)
+      payload[:exp] = exp.to_i
+      JWT.encode(payload, Rails.application.secrets.secret_key_base)
+    end
 
-   def decode(token)
-     body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
-     HashWithIndifferentAccess.new body
-   rescue
-     nil
-   end
- end
+    def decode(token)
+      body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+      HashWithIndifferentAccess.new body
+    rescue StandardError
+      nil
+    end
+  end
 end
