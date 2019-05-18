ea737f019f9e5b471af1770f297189e6ed981720
federicomoyamartin <federicomoyamartin@gmail.com>
2019-05-18 19:09:00 -0300

    Update endpoints

--- app/resources/rest/v1/organization_resource.rb
+++ app/resources/rest/v1/organization_resource.rb
@@ -1,9 +1,18 @@
 module Rest
   module V1
     class OrganizationResource < ApplicationResource
-      attributes :name, :description, :user_id
+      attributes :name, :description, :user_id, :avatar_url, :users_count
       
       has_one :user
+
+      def avatar_url
+        "http://www.saleclickonline.cl/wp-content/uploads/2017/02/nuevo-logo-google.jpeg"
+      end
+
+      # only active users
+      def users_count
+        23
+      end
 
     end
   end
--- app/resources/rest/v1/user_resource.rb
+++ app/resources/rest/v1/user_resource.rb
@@ -1,7 +1,7 @@
 module Rest
   module V1
     class UserResource < ApplicationResource
-      attributes :email, :first_name, :last_name, :password, :user_type, :created_at, :avatar_url
+      attributes :email, :first_name, :last_name, :password, :user_type, :created_at, :avatar_url, :organization_id
 
       has_one :organizations
       has_many :decisions
