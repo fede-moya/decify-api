32c875acd43cfe57bc9cb252f14d9e0ddffa1cbe
federicomoyamartin <federicomoyamartin@gmail.com>
2019-05-18 18:33:03 -0300

    Complete invitaiton service

--- app/services/invitation_service.rb
+++ app/services/invitation_service.rb
@@ -1,5 +1,4 @@
 class InvitationService
-
   attr_accessor :organization, :emails
 
   def initialize(args)
@@ -8,5 +7,29 @@ class InvitationService
   end
 
   def send_invitations
+    emails.each do |email|
+      user = User.create!(
+        email: email,
+        organization: organization,
+        authorization_code: SecureRandom.hex(10)
+      )
+
+      send_invitation user
+    end
+  end
+
+  def send_invitation(user)
+    branch_service = BranchService.new
+    deep_link = branch_service.get_client.link(
+      channel: 'decify-api',
+      feature: 'login',
+      data: {
+        code:  user.authorization_code,
+        email: user.email,
+        first_login: true
+      }
+    ).url
+
+    UserMailer.delay.new_mobile_session(user, deep_link)
   end
 end
