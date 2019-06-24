43337e7aad7b087e32b6a7d644670384360334f2
federicomoyamartin <federicomoyamartin@gmail.com>
2019-06-23 21:23:15 -0300

    Update production seeds to create at least on decision

--- db/seeds/production.rb
+++ db/seeds/production.rb
@@ -1,3 +1,4 @@
+# load(Rails.root.join( 'db', 'seeds', "#{Rails.env.downcase}.rb"))
 pablo_garin = User.create(first_name: 'Pablo', last_name: 'Garin', email: 'pablo.garin@gmail.com', user_type: :master, password: 'abcd1234')
 pablo_garin.avatar.attach(io: File.open('db/seeds/avatars/avatar-pablo.png'), filename: 'avatar-pablo.png')
 
@@ -65,3 +66,34 @@ users.each do |user|
 
   u.avatar.attach(io: File.open("db/seeds/avatars/avatar-#{user[2]}.jpg"), filename: "avatar-#{user[2]}.jpg")
 end
+
+decision_type_democratic = DecisionType.create(name: 'Democratica', code: 0)
+
+[ 'gerencia', 'proyecto UCUDAL', 'diseño', 'desarrollo', 'testing', 'scrum', 'retrospective', 'daily', 'planning', 'project manager'].each { |tag| Tag.create(text: tag) }
+
+
+cecilia = User.find_by(email: 'cecilia.alvarez@gmail.com')
+
+d1 = Decision.create(
+  user: cecilia,
+  decision_type: decision_type_democratic,
+  title: '¿ Quién debe ser el project manager del nuevo proyecto ?',
+  description: 'Todos los projects managers de la compañia están actualmente ocupados en proyectos. Tenemos que elegir a alguien que sea bueno para manejar múltiples proyectos.'
+)
+
+d1_a1 = d1.alternatives.create(title: 'Ezequiel Rodriguez')
+d1_a2 = d1.alternatives.create(title: 'Ana Laferte')
+d1_a3 = d1.alternatives.create(title: 'Andrea Esperdutti')
+
+users_for_d1 = ['manuela.quintela@gmail.com', 'guido.rodriguez@gmail.com', 'nicolas.machado@gmail.com', 'agustina.trigo@gmail.com']
+users_for_d1.each { |user_email| UserDecision.create(decision: d1, user: User.find_by(email: user_email) ) }
+
+
+DecisionTag.create(decision: d1, tag: Tag.find_by(text: 'proyecto UCUDAL'))
+DecisionTag.create(decision: d1, tag: Tag.find_by(text: 'gerencia'))
+DecisionTag.create(decision: d1, tag: Tag.find_by(text: 'project manager'))
+
+Vote.create(alternative: d1_a1, user: User.find_by(email: 'manuela.quintela@gmail.com'))
+Vote.create(alternative: d1_a1, user: User.find_by(email: 'guido.rodriguez@gmail.com'))
+Vote.create(alternative: d1_a2, user: User.find_by(email: 'nicolas.machado@gmail.com'))
+Vote.create(alternative: d1_a3, user: User.find_by(email: 'agustina.trigo@gmail.com'))
