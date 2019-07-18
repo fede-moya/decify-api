f3ce27163cf03689c25faaafe0626d724c6ca278
Federico Moyá <federicomoyamartin@gmail.com>
2019-07-18 14:30:08 -0300

    Add endpoints to like/dislike messages (#37)

--- app/controllers/rest/v1/messages_controller.rb
+++ app/controllers/rest/v1/messages_controller.rb
@@ -3,6 +3,29 @@
 module Rest
   module V1
     class MessagesController < ApiController
+      before_action :message, only: %i[like dislike]
+      before_action :validate_current_user, only: %i[like dislike]
+
+      def like
+        @message.increment!(:likes_count)
+        head :ok
+      end
+
+      def dislike
+        @message.increment!(:dislikes_count)
+        head :ok
+      end
+
+      private
+
+      def validate_current_user
+        head :forbbiden unless current_user.organization_id.eql?(@message.decision.user.organization.id)
+      end
+
+      def message
+        @message = Message.find_by(id: params[:id])
+        head :no_found unless @message
+      end
     end
   end
 end
